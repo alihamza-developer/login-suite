515b64f232f6d3896cde3002e95b73541bfbc45c
alihamza-developer <alihamzaofficial3536@gmail.com>
2024-07-29 16:59:29 -0700

    delete icons manager js files

--- js/IconsManager/index.js
+++
@@ -1,7 +0,0 @@
-fn.cb.exportIconsCB = ($el, res) => {
-    let success = handleAlert(res);
-    if (!success) return false;
-    res = JSON.parse(res);
-    ICONS_MANAGER.exportIcons(res.data);
-    return false;
-}
